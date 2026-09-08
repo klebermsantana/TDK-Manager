afe9ae2c63bb82274fc9b354d92fbef054df51f5
klebermsantana <52982901+klebermsantana@users.noreply.github.com>
2026-09-08 20:16:52 -0300

    feat: aplicar regras de abertura dos chamados

---
+++ drizzle/0022_fast_lady_bullseye.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `service_calls` ADD `service_taker` text;--> statement-breakpoint
+ALTER TABLE `service_calls` ADD `request_origin` text;--> statement-breakpoint
+ALTER TABLE `service_calls` ADD `department` text;--> statement-breakpoint
+ALTER TABLE `service_calls` ADD `customer_ticket` text;
