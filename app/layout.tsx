49de386bba9de32f868c1fa46ac5000b54693892
klebermsantana <52982901+klebermsantana@users.noreply.github.com>
2026-09-11 16:39:55 -0300

    feat: ativa previsão de capacidade da tesouraria

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ import "./treasury-productivity-dashboard.css";
 import "./treasury-capacity-dashboard.css";
 import "./treasury-routing-profiles-dashboard.css";
 import "./treasury-routing-schedules-dashboard.css";
+import "./treasury-capacity-forecast-dashboard.css";
 import "./treasury-dashboard.css";
 import "./treasury-reconciliation-dashboard.css";
 import "./treasury-reconciliation-split-dashboard.css";
@@ -55,6 +56,7 @@ import { TreasuryProductivityDashboard } from "./treasury-productivity-dashboard
 import { TreasuryCapacityDashboard } from "./treasury-capacity-dashboard";
 import { TreasuryRoutingProfilesDashboard } from "./treasury-routing-profiles-dashboard";
 import { TreasuryRoutingSchedulesDashboard } from "./treasury-routing-schedules-dashboard";
+import { TreasuryCapacityForecastDashboard } from "./treasury-capacity-forecast-dashboard";
 import { TreasuryDashboard } from "./treasury-dashboard";
 import { TreasuryReconciliationDashboard } from "./treasury-reconciliation-dashboard";
 import { TreasuryReconciliationSplitDashboard } from "./treasury-reconciliation-split-dashboard";
@@ -104,6 +106,7 @@ export default function RootLayout({
         <TreasuryCapacityDashboard />
         <TreasuryRoutingProfilesDashboard />
         <TreasuryRoutingSchedulesDashboard />
+        <TreasuryCapacityForecastDashboard />
         <TreasuryDashboard />
         <TreasuryReconciliationDashboard />
         <TreasuryReconciliationSplitDashboard />
